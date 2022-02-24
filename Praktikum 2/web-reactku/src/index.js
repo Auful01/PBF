432e1528216ecc403d55042afceaea0b0540898a
auful01 <distriq19@gmail.com>
2022-02-24 12:59:05 +0700

    praktikum2-update

--- Praktikum 2/web-reactku/src/index.js
+++ Praktikum 2/web-reactku/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './text.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,30 +10,45 @@ function Hello() {
     <div className='Hello'>
 
       <div class="row d-flex justify-content-center " style={{ marginTop: '10%' }}>
-        <h1 class="font-weight-bold">Hello World</h1>
-      </div>
-      <div class="row d-flex justify-content-center mt-3">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-          Launch demo modal
-        </button>
-        <div class="modal fade " style={{ background: 'rgba(255, 255, 255, 0.6)', backdropFilter: 'blur(5px) ', WebkitBackdropFilter: 'blur(5px)' }} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog border-0 modal-dialog-centered" >
-            <div class="modal-content border-0" >
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Greetings</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <div class="text-center">
-                  <h3 class="font-weight-bold">Hello World!</h3>
-                  <p>My Name Is Auful Kirom</p>
+        <div class="card shadow border-0 p-5">
+          <div class="card-body">
+            <h1 class="font-weight-bold">Hello World</h1>
+
+            <div class="row d-flex justify-content-center mt-3">
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                <i class="fas fa-paper-plane"></i>
+              </button>
+            </div>
+
+
+
+            <div class="modal fade " style={{ background: 'rgba(255, 255, 255, 0.5)', backdropFilter: 'blur(5px) ', WebkitBackdropFilter: 'blur(5px)' }} id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog border-0 modal-dialog-centered" >
+                <div class="modal-content border-0" >
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Greetings</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="text-center">
+                      <h3 class="font-weight-bold">Hello World!</h3>
+                      <p>My Name Is Auful Kirom</p>
+                    </div>
+                  </div>
+
                 </div>
               </div>
-
             </div>
           </div>
+        </div>
+
+
+      </div>
+      <div class="text-right" >
+        <div class="running-text" style={{ animation: 'slide-left 10s linear infinite', right: '-2000px' }}>
+          <p>Hello, I'm Muhammad Auful Kirom</p>
         </div>
       </div>
     </div>
